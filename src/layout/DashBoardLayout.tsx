2af8dfb64da5b4a07beabaa34f81dbce9dfe438f
Suresh Adhikari <sureshadhikari2002@gmail.com>
2024-06-05 23:26:57 +0530

    show books table

--- src/layout/DashBoardLayout.tsx
+++ src/layout/DashBoardLayout.tsx
@@ -1,4 +1,4 @@
-import { Badge } from '@/components/ui/badge';
+
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 import {
@@ -16,13 +16,12 @@ import {
     Bell,
     CircleUser,
     Home,
-    LineChart,
+   
     Menu,
     Package,
     Package2,
     Search,
-    ShoppingCart,
-    Users,
+    
 } from 'lucide-react';
 import { Link, Navigate, NavLink, Outlet } from 'react-router-dom';
 
@@ -43,7 +42,7 @@ const DashboardLayout = () => {
             <div className="hidden border-r bg-muted/40 md:block">
                 <div className="flex h-full max-h-screen flex-col gap-2">
                     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                        <Link to="/" className="flex items-center gap-2 font-semibold">
+                        <Link to="/dashboard/home" className="flex items-center gap-2 font-semibold">
                             <Package2 className="h-6 w-6" />
                             <span className="">Coder's Book</span>
                         </Link>
@@ -107,44 +106,25 @@ const DashboardLayout = () => {
                         <SheetContent side="left" className="flex flex-col">
                             <nav className="grid gap-2 text-lg font-medium">
                                 <Link
-                                    to="#"
+                                    to="/dashboard/home"
                                     className="flex items-center gap-2 text-lg font-semibold">
                                     <Package2 className="h-6 w-6" />
-                                    <span className="sr-only">Acme Inc</span>
+                                    <span className="sr-only">Coder's Book</span>
                                 </Link>
                                 <Link
-                                    to="#"
+                                    to="/dashboard/home"
                                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
                                     <Home className="h-5 w-5" />
-                                    Dashboard
+                                    Home
                                 </Link>
+                              
                                 <Link
-                                    to="#"
-                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
-                                    <ShoppingCart className="h-5 w-5" />
-                                    Orders
-                                    <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                        6
-                                    </Badge>
-                                </Link>
-                                <Link
-                                    to="#"
+                                    to="/dashboard/books"
                                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
                                     <Package className="h-5 w-5" />
-                                    Products
+                                    Books
                                 </Link>
-                                <Link
-                                    to="#"
-                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                                    <Users className="h-5 w-5" />
-                                    Customers
-                                </Link>
-                                <Link
-                                    to="#"
-                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                                    <LineChart className="h-5 w-5" />
-                                    Analytics
-                                </Link>
+                                
                             </nav>
                             <div className="mt-auto">
                                 <Card>
